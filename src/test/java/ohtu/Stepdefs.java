53203edc10808145d33e066c15b49671aa2d40bb
EssKayz <esskay@protonmail.com>
2018-11-20 12:46:19 +0200

    Continued travis fixing
    Turns out Travis doesn't like Chrome. Add bubble gum.

--- src/test/java/ohtu/Stepdefs.java
+++ src/test/java/ohtu/Stepdefs.java
@@ -10,6 +10,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Stepdefs {
 
@@ -25,7 +26,11 @@ public class Stepdefs {
         String absolutePath = file.getAbsolutePath();
         System.setProperty("webdriver.gecko.driver", absolutePath);
 
-        this.driver = new ChromeDriver();
+        if (System.getProperty("os.name").matches("Windows 10")) {
+            this.driver = new ChromeDriver();
+        } else {
+            this.driver = new FirefoxDriver();
+        }
     }
 
     @After
@@ -35,17 +40,17 @@ public class Stepdefs {
 
     @Given("^user is at the main page$")
     public void user_is_at_the_main_page() throws Throwable {
-        driver.get("http://localhost:" + 8080 + "/" );
-        Thread.sleep(1000);        
+        driver.get("http://localhost:" + 8080 + "/");
+        Thread.sleep(1000);
     }
 
     @When("^a link is clicked$")
     public void a_link_is_clicked() throws Throwable {
-        Thread.sleep(1000);  
-        clickLinkWithText("linkki" );
-        Thread.sleep(1000);  
-    }    
-   
+        Thread.sleep(1000);
+        clickLinkWithText("linkki");
+        Thread.sleep(1000);
+    }
+
     @Then("^\"([^\"]*)\" is shown$")
     public void is_shown(String arg1) throws Throwable {
         assertTrue(driver.findElement(By.tagName("body"))
@@ -54,15 +59,15 @@ public class Stepdefs {
 
     private void clickLinkWithText(String text) {
         int trials = 0;
-        while( trials++<5 ) {
-            try{
+        while (trials++ < 5) {
+            try {
                 WebElement element = driver.findElement(By.linkText(text));
                 element.click();
-                break;           
-            } catch(Exception e) {
+                break;
+            } catch (Exception e) {
                 System.out.println(e.getStackTrace());
             }
         }
     }
-    
+
 }
