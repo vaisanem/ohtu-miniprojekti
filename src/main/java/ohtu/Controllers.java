abd35958936b48fb875ec34f64784cd189e7aa96
vaisanem <miikkamv@gmail.com>
2018-11-25 21:07:34 +0200

    defining add for blogs

--- src/main/java/ohtu/Controllers.java
+++ src/main/java/ohtu/Controllers.java
@@ -2,7 +2,6 @@ package ohtu;
 
 import java.sql.SQLException;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 import ohtu.db.ItemTypeManager;
 import ohtu.types.*;
@@ -32,8 +31,8 @@ public class Controllers {
         if (user.length() < 1) {
             user = "default";
         }
-        List<ItemType> books = itemMan.findAll(user);
-        model.addAttribute("items", books);
+        List<ItemType> items = itemMan.findAll(user);
+        model.addAttribute("items", items);
         return "itemView";
     }
 
@@ -94,30 +93,13 @@ public class Controllers {
                     itemMan.getBlogMan().add(blog, user);
                     return "redirect:/items";
                 } catch (Exception e) {
-                    //model.addAttribute("error", e.getMessage());
-                    //return "newItem";
-                    return "error";
+                    model.addAttribute("error", e.getMessage());
+                    return "newItem";
+                    //return "error";
                 }
             }
 
             default: {
-                return "error";
-            }
-        }
-
-    }
-    
-    @PostMapping("/addItem2")
-    public String addItem2(ModelMap model, RedirectAttributes userAttribute, @RequestParam Map<String, String> params) throws SQLException {
-        String type = params.get("type");
-        switch (type) {
-            case "book": {
-                return addBook(model, userAttribute, params.get("bookTitle"), params.get("isbn"), params.get("year"), params.get("author"), params.get("user"));
-            } case "video": {
-                return addVideo(model, userAttribute, params.get("videoTitle"), params.get("videoURL"), params.get("videoPoster"), params.get("user"));
-            } case "blog": {
-                return addBlog(model, userAttribute, params.get("blogTitle"), params.get("blogURL"), params.get("blogPoster"), params.get("user"));
-            } default: {
                 return "error";
             }
         }
@@ -133,7 +115,7 @@ public class Controllers {
 
     @RequestMapping(value = "/blog/{id}", method = RequestMethod.GET)
     public String blog(@PathVariable int id, ModelMap model) throws SQLException {
-        Blog blog = null;  // TODO -> missing manager.
+        Blog blog = itemMan.getBlogMan().findOne(id);
         model.addAttribute("blog", blog);
         return "blog";
     }
@@ -145,52 +127,4 @@ public class Controllers {
         return "video";
     }
     
-    private String addBook(ModelMap model, RedirectAttributes userAttribute, String title, String isbn, String year, String author, String user) {
-        int intYear;
-        if (!year.matches("[0-9]+")) {
-            model.addAttribute("error", "year not numeric");
-            return "error";
-        } else {
-            intYear = Integer.parseInt(year);
-        }
-        userAttribute.addFlashAttribute("user", user);
-        System.out.println("Redirected user : " + user);
-        Book book = new Book(isbn, title, author, intYear);
-                
-        try {
-            itemMan.getBookMan().add(book, user);
-            return "redirect:/items";
-        } catch (Exception e) {
-            model.addAttribute("error", e.getMessage());
-            return "error";
-        }
-    }
-
-    private String addVideo(ModelMap model, RedirectAttributes userAttribute, String URL, String title, String poster, String user) {
-        try {
-            userAttribute.addFlashAttribute("user", user);
-            System.out.println("Redirected user : " + user);
-            Video vid = new Video(URL, title, poster);
-            itemMan.getVideoMan().add(vid, user);
-            return "redirect:/items";
-        } catch (Exception e) {
-            model.addAttribute("error", e.getMessage());
-            //return "newItem";
-            return "error";
-        } 
-    }
-    
-    private String addBlog(ModelMap model, RedirectAttributes userAttribute, String URL, String title, String poster, String user) {
-        try {
-            userAttribute.addFlashAttribute("user", user);
-            System.out.println("Redirected user : " + user);
-            Blog blog = new Blog(URL, title, poster);
-            itemMan.getBlogMan().add(blog, user);
-            return "redirect:/items";
-        } catch (Exception e) {
-            model.addAttribute("error", e.getMessage());
-            //return "newItem";
-            return "error";
-        }        
-    }
 }
