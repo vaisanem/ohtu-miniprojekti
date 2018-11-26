c172c429da1da62fb52c714f01fd67e3a6b45dfd
xTooth <32893884+xTooth@users.noreply.github.com>
2018-11-27 00:10:38 +0200

    Update Video.java
    MODIFIED> oops. I derped. now correct method is commented.

--- src/main/java/ohtu/types/Video.java
+++ src/main/java/ohtu/types/Video.java
@@ -45,27 +45,27 @@ public class Video extends ItemType {
         super.setTitle(title);
 
     }
-
-    public Video(String URL) {
-        super.setType(typeIdentifier.video);
-        if (URL.contains("=")) {
-            this.URL = URL.substring(URL.indexOf('=') + 1);
-        } else if (URL.contains("youtu.be")) {
-            this.URL = URL.substring(URL.indexOf('.') + 4);
-        } else {
-            this.URL = URL;
-        }
-        // TODO Fetch information from YouTubeAPI.
-    }
 // Waiting for implementation
-//    public Video(ResultSet rs) throws SQLException {
+//    public Video(String URL) {
 //        super.setType(typeIdentifier.video);
-//        super.setId(rs.getInt("id"));
-//        super.setTitle(rs.getString("title"));
-//        URL = rs.getString("URL");
-//        poster = rs.getString("Author");
-//
+//        if (URL.contains("=")) {
+//            this.URL = URL.substring(URL.indexOf('=') + 1);
+//        } else if (URL.contains("youtu.be")) {
+//            this.URL = URL.substring(URL.indexOf('.') + 4);
+//        } else {
+//            this.URL = URL;
+//        }
+//        // TODO Fetch information from YouTubeAPI.
 //    }
+
+    public Video(ResultSet rs) throws SQLException {
+        super.setType(typeIdentifier.video);
+        super.setId(rs.getInt("id"));
+        super.setTitle(rs.getString("title"));
+        URL = rs.getString("URL");
+        poster = rs.getString("Author");
+
+    }
 
     public String getURL() {
         return URL;
