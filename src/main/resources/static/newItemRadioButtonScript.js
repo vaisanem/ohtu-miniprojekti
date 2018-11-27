9e24ce79c9446cfcd0b3bbb8751811bdc66f90a1
xTooth <32893884+xTooth@users.noreply.github.com>
2018-11-27 11:52:24 +0200

    Update newItemRadioButtonScript.js
    MODIFIED> js file to use double quotes to make codacy shut up.
    
    "A JavaScript string stores a series of characters like "John Doe". A string can be any text inside double or single quotes: var carname = "Volvo XC60"; var carname = 'Volvo XC60' "    .....     -.-

--- src/main/resources/static/newItemRadioButtonScript.js
+++ src/main/resources/static/newItemRadioButtonScript.js
@@ -1,19 +1,19 @@
 function WhichCheck() {
     
-    if (document.getElementById('book').checked) {
-        document.getElementById('bookAdd').style.display = 'block';
-        document.getElementById('blogAdd').style.display = 'none';
-        document.getElementById('videoAdd').style.display = 'none';
+    if (document.getElementById("book").checked) {
+        document.getElementById("bookAdd").style.display = "block";
+        document.getElementById("blogAdd").style.display = "none";
+        document.getElementById("videoAdd").style.display = "none";
         
-    } else if (document.getElementById('blog').checked) {
-        document.getElementById('blogAdd').style.display = 'block';
-        document.getElementById('bookAdd').style.display = 'none';
-        document.getElementById('videoAdd').style.display = 'none';
+    } else if (document.getElementById("blog").checked) {
+        document.getElementById("blogAdd").style.display = "block";
+        document.getElementById("bookAdd").style.display = "none";
+        document.getElementById("videoAdd").style.display = "none";
         
     } else {
-        document.getElementById('videoAdd').style.display = 'block';
-        document.getElementById('bookAdd').style.display = 'none';
-        document.getElementById('blogAdd').style.display = 'none';
+        document.getElementById("videoAdd").style.display = "block";
+        document.getElementById("bookAdd").style.display = "none";
+        document.getElementById("blogAdd").style.display = "none";
     }
 }
 
